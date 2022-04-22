3b3c4f805cea40ca249995cc1c80b371e0ad3911
joshua <39487800+sjstalin@users.noreply.github.com>
2022-04-22 18:37:15 +0530

    Add files via upload

---
+++ serverindex.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  user: "sqluser",
+  host: "127.0.0.1",
+  port: "3306",
+  password: "password",
+  database: "employeeSystem",
+});
+
+app.post("/create", (req, res) => {
+  const name = req.body.name;
+  const age = req.body.age;
+  const state = req.body.state;
+  const type = req.body.type;
+  const adnumber = req.body.adnumber;
+
+  db.query(
+    "INSERT INTO employees (name, age, state, type, adnumber) VALUES (?,?,?,?,?)",
+    [name, age, state, type, adnumber],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values Inserted");
+      }
+    }
+  );
+});
+
+app.get("/employees", (req, res) => {
+  db.query("SELECT * FROM employees", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.put("/update", (req, res) => {
+  const id = req.body.id;
+  const adnumber = req.body.adnumber;
+  db.query(
+    "UPDATE employees SET adnumber = ? WHERE id = ?",
+    [adnumber, id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+app.delete("/delete/:id", (req, res) => {
+  const id = req.params.id;
+  db.query("DELETE FROM employees WHERE id = ?", id, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.listen(3001, () => {
+  console.log("Yey, your server is running on port 3001");
+});
