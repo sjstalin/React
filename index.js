e1f9c0ea060dc9e5f2f95f1b7eb33f26b6c78875
joshua <39487800+sjstalin@users.noreply.github.com>
2022-04-22 18:31:46 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,76 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-  // resolve.fallback: { "url": false }
-);
+app.use(cors());
+app.use(express.json());
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const db = mysql.createConnection({
+  user: "sqluser",
+  host: "127.0.0.1",
+  port: "3306",
+  password: "password",
+  database: "employeeSystem",
+});
+
+app.post("/create", (req, res) => {
+  const name = req.body.name;
+  const age = req.body.age;
+  const state = req.body.state;
+  const type = req.body.type;
+  const adnumber = req.body.adnumber;
+
+  db.query(
+    "INSERT INTO employees (name, age, state, type, adnumber) VALUES (?,?,?,?,?)",
+    [name, age, state, type, adnumber],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values Inserted");
+      }
+    }
+  );
+});
+
+app.get("/employees", (req, res) => {
+  db.query("SELECT * FROM employees", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.put("/update", (req, res) => {
+  const id = req.body.id;
+  const adnumber = req.body.adnumber;
+  db.query(
+    "UPDATE employees SET adnumber = ? WHERE id = ?",
+    [adnumber, id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+app.delete("/delete/:id", (req, res) => {
+  const id = req.params.id;
+  db.query("DELETE FROM employees WHERE id = ?", id, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.listen(3001, () => {
+  console.log("Yey, your server is running on port 3001");
+});
